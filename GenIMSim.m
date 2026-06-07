e3f79c610d1f79632c4bcfea5e50d1408114a53b
Klaus Oberauer <k.oberauer@psychologie.uzh.ch>
2026-06-07 06:14:37 +0200

    integrated CD and reproduction into a single call to IMSim, using the two response values

--- GenIMSim.m
+++ GenIMSim.m
@@ -59,7 +59,7 @@ model = 1;  % 1 = IMSim
 % 47 = Generic Parameter-Sensitivity simulation for change detection (simultaneous, set-size 6)
 
 saveResults = 0;
-Exp = 35;
+Exp = 32;
 
 Setsize = 6;  % default value (can be overwritten later)
 fitMM = 0;   % fit mixture model?
@@ -67,8 +67,8 @@ fitIMSim = 0; % fit IM?
 
 %%% Experimental Constants/Defaults
 
-E.ntrials = 100;     % number of trials to run per subject and condition
-E.nsubj = 20;        % number of subjects
+E.ntrials = 10;     % number of trials to run per subject and condition
+E.nsubj = 10;        % number of subjects
 E.ngroups = 1;       % number of groups of subjects
 E.material = 1;      % 1 = features on a continuous circular dimension (e.g., color wheel); 2 = highly distinct features; 3 = orientations with 180 degree scale
 E.targetDim = 1;     % feature dimension of the target stimuli: 1 = color, 2 = orientation, 3 = spatial location
